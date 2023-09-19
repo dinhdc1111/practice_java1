3cee56f76e904360d97d476e742ebda390aea0fd
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-09-19 19:13:36 +0700

    feat: :fire: add to FizzBuzz

---
+++ lab1/FizzBuzz.java
@@ -0,0 +1,25 @@
+package lab1;
+
+import java.util.Scanner;
+
+/**
+ * FizzBuzz
+ */
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Mời nhập số: ");
+        int choiceNumber = sc.nextInt();
+        if (choiceNumber % 3 == 0 && choiceNumber % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (choiceNumber % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (choiceNumber % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(choiceNumber);
+        }
+        sc.close();
+    }
+}
